1e3ef0b83d01b003826e007e777e29f9030df2df
Duane Dibley <dibley1973@yahoo.co.uk>
2015-10-29 13:34:26 +0000

    Update CharacterBufferTests.cs

--- Tests/ElementsTests/Tests/CharacterBufferTests.cs
+++ Tests/ElementsTests/Tests/CharacterBufferTests.cs
@@ -1,6 +1,6 @@
-﻿using System;
 using Elements;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ElementsTests.Tests
 {
@@ -17,8 +17,9 @@ namespace ElementsTests.Tests
             string data = string.Empty;
 
             // ACT
+            // ReSharper disable once ObjectCreationAsStatement
             new CharacterBuffer(data);
-            
+
             // ASSERT
             // Exception thrown by here!
         }
@@ -42,6 +43,22 @@ namespace ElementsTests.Tests
             Assert.AreEqual(expectedCharacter, actualCharacter);
         }
 
+        [TestMethod]
+        public void CurrentCharacter_ReturnsNullCharacterOfData_FaterMoveToEndIsCalled()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            const char expectedCharacter = CharacterBuffer.NullCharacter;
+
+            // ACT
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+            var actualCharacter = characterBuffer.CurrentCharacter;
+
+            // ASSERT
+            Assert.AreEqual(expectedCharacter, actualCharacter);
+        }
+
         #endregion
 
         #region CurrentIndexPosition
@@ -55,6 +72,39 @@ namespace ElementsTests.Tests
 
             // ACT
             var characterBuffer = new CharacterBuffer(data);
+            var actualCurrentPosition = characterBuffer.CurrentIndexPosition;
+
+            // ASSERT
+            Assert.AreEqual(expectedCurrentPosition, actualCurrentPosition);
+        }
+
+        [TestMethod]
+        public void CurrentIndexPosition_ReturnsLastPosition_WhenClassConstructedWithDataAndMoveToEndCalled()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            int expectedIndexPosition = data.Length;
+
+            // ACT
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+            var actualIndexPosition = characterBuffer.CurrentIndexPosition;
+
+            // ASSERT
+            Assert.AreEqual(expectedIndexPosition, actualIndexPosition);
+        }
+
+        [TestMethod]
+        public void CurrentIndexPosition_ReturnsZero_WhenClassConstructedWithDataAndMoveToStartCalledAfterMoveToEnd()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            const int expectedCurrentPosition = 0;
+
+            // ACT
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+            characterBuffer.MoveToStart();
             var actualCurrentPosition = characterBuffer.CurrentIndexPosition;
 
             // ASSERT
@@ -89,13 +139,29 @@ namespace ElementsTests.Tests
             // ARRANGE
             const string data = Fakes.Literal.BasicLiteral;
             var characterBuffer = new CharacterBuffer(data);
-                
+
             // ACT
             var actualData = characterBuffer.GetToEnd();
             var actualDataLength = actualData.Length;
 
             // ASSERT
             Assert.AreEqual(data.Length, actualDataLength);
+        }
+
+        [TestMethod]
+        public void GetToEnd_ReturnsEmptyString_AfterMoveToEndIsCalled()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+            string expectedCharachter = string.Empty;
+
+            // ACT
+            characterBuffer.MoveToEnd();
+            string actualData = characterBuffer.GetToEnd();
+            
+            // ASSERT
+            Assert.AreEqual(actualData, expectedCharachter);
         }
 
         #endregion
@@ -110,14 +176,14 @@ namespace ElementsTests.Tests
 
             // ACT
             var characterBuffer = new CharacterBuffer(data);
-            var isAtBeginning = characterBuffer.IsAtBeginning;
+            var isAtBeginning = characterBuffer.IsAtStart;
 
             // ASSERT
             Assert.IsTrue(isAtBeginning);
         }
 
         #endregion
-        
+
         #region IsAtEnd
 
         [TestMethod]
@@ -153,6 +219,23 @@ namespace ElementsTests.Tests
             Assert.AreEqual(expectedCharacter, actualCharacter);
         }
 
+
+        [TestMethod]
+        public void NextCharacter_ReturnsNullCharacter_AfterMoveToEndIsCalled()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+            const char expectedCharacter = CharacterBuffer.NullCharacter;
+
+            // ACT
+            characterBuffer.MoveToEnd();
+            var actualCharacter = characterBuffer.NextCharacter;
+
+            // ASSERT
+            Assert.AreEqual(actualCharacter, expectedCharacter);
+        }
+
         #endregion
 
         #region PreviousCharacter
@@ -172,6 +255,174 @@ namespace ElementsTests.Tests
             Assert.AreEqual(expectedCharacter, actualCharacter);
         }
 
+        [TestMethod]
+        public void PreviousCharacter_ReturnslastCharacter_AfterMoveToEnd()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            char expectedCharacter = data.ToCharArray(data.Length - 1, 1)[0];
+
+            // ACT
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+            var actualCharacter = characterBuffer.PreviousCharacter;
+
+            // ASSERT
+            Assert.AreEqual(expectedCharacter, actualCharacter);
+        }
+
         #endregion
+
+        #region MoveNext
+
+        [TestMethod]
+        public void MoveNext_ReturnsTrue_WhenIndexIsNotAtEnd()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            var result = characterBuffer.MoveNext();
+
+            // ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void MoveNext_ReturnsFalse_WhenIndexIsAtEnd()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+
+            // ACT
+            var result = characterBuffer.MoveNext();
+
+            // ASSERT
+            Assert.IsFalse(result);
+        }
+
+        #endregion
+
+        #region MovePrevious
+
+        [TestMethod]
+        public void MovePrevious_ReturnsTrue_WhenIndexIsNotAtStart()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+            characterBuffer.MoveToEnd();
+
+            // ACT
+            var result = characterBuffer.MovePrevious();
+
+            // ASSERT
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void MovePrevious_ReturnsFalse_WhenIndexIsAtStart()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            var result = characterBuffer.MovePrevious();
+
+            // ASSERT
+            Assert.IsFalse(result);
+        }
+
+        #endregion
+
+
+        #region MoveToEnd
+
+
+        #endregion
+
+        #region MoveToStart
+
+
+        #endregion
+
+        #region Substring
+
+        [TestMethod]
+        public void Substring_ReturnsCorrectPortionOfData()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            int expectedStartIndex = 1;
+            int expectedLength = 1;
+            string expected = data.Substring(expectedStartIndex, expectedLength);
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            var actual = characterBuffer.Substring(expectedStartIndex, expectedLength);
+
+            // ASSERT
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Substring_ArgumentOutOfRangeExceptionThrown_WhenStartIndexIsLessThanZero()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            const int expectedStartIndex = -1;
+            const int expectedLength = 1;
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            characterBuffer.Substring(expectedStartIndex, expectedLength);
+
+            // ASSERT
+            // Exception thrown by now
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Substring_ArgumentOutOfRangeExceptionThrown_WhenStartIndexPlusLengthIsGreaterThanLength()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            const int expectedStartIndex = 1;
+            const int expectedLength = 10;
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            characterBuffer.Substring(expectedStartIndex, expectedLength);
+
+            // ASSERT
+            // Exception thrown by now
+        }
+
+
+        #endregion
+
+        #region ToString
+
+        [TestMethod]
+        public void ToString_ReturnsSameAsOriginalData()
+        {
+            // ARRANGE
+            const string data = Fakes.Literal.BasicLiteral;
+            var characterBuffer = new CharacterBuffer(data);
+
+            // ACT
+            var actual = characterBuffer.ToString();
+
+            // ASSERT
+            Assert.AreEqual(data, actual);
+        }
+
+        #endregion
+
     }
 }
